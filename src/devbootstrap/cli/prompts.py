367e89f1ac7492d6e59a4bb0c0256dae30e86aa5
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-06-05 14:49:08 +0530

    Feat: CLI generation

--- src/devbootstrap/cli/prompts.py
+++ src/devbootstrap/cli/prompts.py
@@ -6,8 +6,17 @@ from devbootstrap.core.models import ProjectConfig
 def get_project_config() -> ProjectConfig:
     project_name = typer.prompt("Project Name")
 
+    template = typer.prompt(
+        "Template (python, fastapi, data-science, ml)"
+    )
+
+    use_git = typer.confirm(
+        "Initialize Git?",
+        default=True,
+    )
+
     return ProjectConfig(
         name=project_name,
-        template="python",
-        use_git=True,
+        template=template,
+        use_git=use_git,
     )
