cf7d688e750c0417177f501c99a6cdb51acc8924
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-06-05 19:15:29 +0530

    Polish: Templates CLI

--- src/spawn/cli/prompts.py
+++ src/spawn/cli/prompts.py
@@ -1,25 +1,41 @@
 import typer
+from rich.table import Table
 
+from spawn.utils.console import console
 from spawn.core.models import ProjectConfig
+
+
+TEMPLATE_CHOICES = {
+    "1": "python",
+    "2": "fastapi",
+    "3": "data-science",
+    "4": "ml",
+}
 
 
 def get_project_config() -> ProjectConfig:
     project_name = typer.prompt("Project Name")
 
-    TEMPLATE_CHOICES = {
-    "1": "python",
-    "2": "fastapi",
-    "3": "data-science",
-    "4": "ml",
-    }
-    menu = "\n".join([f" [{k}] {v}" for k, v in TEMPLATE_CHOICES.items()])
-    print(f"\nAvailable Templates:\n{menu}\n")
+    table = Table(title="Available Templates")
 
-    choice = typer.prompt("Choose template")
+    table.add_column("#", justify="center")
+    table.add_column("Template")
+
+    table.add_row("1", "Python Script")
+    table.add_row("2", "FastAPI")
+    table.add_row("3", "Data Science")
+    table.add_row("4", "ML Project")
+
+    console.print(table)
+
+    choice = typer.prompt("Choose Template [1-4]")
 
     while choice not in TEMPLATE_CHOICES:
-        typer.secho(" Invalid choice. Please select a valid number.", fg=typer.colors.RED)
-        choice = typer.prompt("Choose template")
+        typer.secho(
+            "Invalid choice. Please select a valid number.",
+            fg=typer.colors.RED,
+        )
+        choice = typer.prompt("Choose Template [1-4]")
 
     template = TEMPLATE_CHOICES[choice]
 
