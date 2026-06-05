37254de5bc7d5e8b082cb60dd0c783acfbbc4728
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-06-05 14:54:22 +0530

    Polish: Invalid templates are ignored

--- src/devbootstrap/cli/prompts.py
+++ src/devbootstrap/cli/prompts.py
@@ -5,10 +5,23 @@ from devbootstrap.core.models import ProjectConfig
 
 def get_project_config() -> ProjectConfig:
     project_name = typer.prompt("Project Name")
+    VALID_TEMPLATES = [
+    "python",
+    "fastapi",
+    "data-science",
+    "ml",
+    ]
 
     template = typer.prompt(
         "Template (python, fastapi, data-science, ml)"
     )
+
+    while template not in VALID_TEMPLATES:
+      print("Invalid template.")
+    
+      template = typer.prompt(
+        "Template (python, fastapi, data-science, ml)"
+        )
 
     use_git = typer.confirm(
         "Initialize Git?",
